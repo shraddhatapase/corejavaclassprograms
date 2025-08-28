680118695136359a2b3e655c96e35bd3896fe367
shraddhatapase <shraddha.tapase18@gmail.com>
2025-08-28 15:06:31 +0530

    first commit

---
+++ Welcome/src/test/Primeno.java
@@ -0,0 +1,31 @@
+// prime no and composite no from array 
+package test;
+
+public class Primeno {
+
+public static void main(String args[]) {
+	int[] arr= new int[5];
+	arr[0]=7;
+	arr[1]=10;
+	arr[2]=13;
+	arr[3]=51;
+	arr[4]=17;
+	
+		for(int i=0;i<5;i++) {
+		boolean flag= false;
+		int num= arr[i];
+			for(int j=2;j<num;j++) {
+				if(num %j==0) {
+					flag=true;
+				}
+			}
+			if(flag==true) {
+				System.out.println("composite" +arr[i]);
+			}else {
+				System.out.println("prime" +arr[i]);
+			}
+		}
+	}
+		
+}
+
--- Welcome/src/test/Testing.java
+++ Welcome/src/test/Testing.java
@@ -1,8 +1,21 @@
+//prime no program
 package test;
 
 public class Testing {
-	public static void main(String arg[]) {
-		System.out.println("HI");
-	}
+    public static void main(String[] args) {
+        int num = 13;
+        boolean isPrime = true; 
 
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = false;               
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Composite");
+        }
+    }
 }
